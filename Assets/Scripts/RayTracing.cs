e9b407a2725d7e4c0bb6d9843b54227836505e7c
Mateusz Dudek <matidudeq@gmail.com>
2021-11-27 18:30:06 +0100

    Ray Tracing implemented

--- Assets/Scripts/RayTracing.cs
+++ Assets/Scripts/RayTracing.cs
@@ -1,15 +1,61 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace RT
 {
 	[RequireComponent(typeof(Camera))]
 	public class RayTracing : MonoBehaviour
 	{
+		[CustomEditor(typeof(RayTracing))]
+		private class RayTracingEditor : Editor
+		{
+			public override void OnInspectorGUI()
+			{
+				base.OnInspectorGUI();
+				
+				if (GUILayout.Button("Spawn Spheres"))
+				{
+					(target as RayTracing)?.CreateSpheres();
+				}
+			}
+		}
+		
+		private struct Sphere
+		{
+			public Vector3 position;
+			public float radius;
+			public Vector3 albedo;
+			public Vector3 specular;
+		};
+		
 		[SerializeField]
 		private ComputeShader rtComputeShader = null;
 
 		[SerializeField]
 		private Texture skyboxTexture2D = null;
+
+		[SerializeField]
+		private Light light = null;
+
+		[SerializeField]
+		private float minimalSphereRadius = 3.0f;
+
+		[SerializeField]
+		private float maximalSphereRadius = 8.0f;
+
+		[SerializeField]
+		private uint spheresCount = 100;
+
+		[SerializeField]
+		private float spherePlacementAreaRadius = 100.0f;
+
+		[SerializeField]
+		private float wavingSpeed = 10.0f;
+
+		private ComputeBuffer sphereBuffer;
 		
 		private RenderTexture renderTexture = null;
 
@@ -17,9 +63,72 @@ namespace RT
 		
 		private const float threadBlockSize = 8.0f;
 
+		private readonly List<Sphere> spheres = new List<Sphere>();
+
+		private void CreateSpheres()
+		{
+			spheres.Clear();
+			
+			for (int i = 0; i < spheresCount; i++)
+			{
+				float radius = minimalSphereRadius + Random.value * (maximalSphereRadius - minimalSphereRadius);
+				Vector2 randomPos = Random.insideUnitCircle * spherePlacementAreaRadius;
+				Vector3 position = new Vector3(randomPos.x, radius, randomPos.y);
+
+				bool isIntersectingOthers = false;
+				
+				foreach (Sphere other in spheres)
+				{
+					float minDist = radius + other.radius;
+					if (Vector3.SqrMagnitude(position - other.position) < minDist * minDist)
+					{
+						isIntersectingOthers = true;
+						break;
+					}
+				}
+
+				if (isIntersectingOthers)
+				{
+					continue;
+				}
+				
+				Color color = Random.ColorHSV();
+				bool metal = Random.value < 0.5f;
+				Vector3 albedo = new Vector3(color.r, color.g, color.b);
+				color = Random.ColorHSV();
+				Vector3 specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.1f;
+
+				spheres.Add(new Sphere()
+				{
+					position = position,
+					radius = radius,
+					albedo = albedo,
+					specular = specular
+				});
+			}
+			
+			int stride = sizeof(float) * ( 3 * 3 + 1);
+			
+			sphereBuffer = new ComputeBuffer(spheres.Count, stride);
+			sphereBuffer.SetData(spheres);
+		}
+
+		private void AnimateSpheres()
+		{
+			for (int i = 0; i < spheres.Count; i++)
+			{
+				Sphere sphere = spheres[i];
+				sphere.position.y = sphere.radius * 1.5f + Mathf.Sin((Time.unscaledTime + i) * wavingSpeed) * sphere.radius;
+				spheres[i] = sphere;
+			}
+			
+			sphereBuffer.SetData(spheres);
+		}
+		
 		private void Awake()
 		{
 			camera = GetComponent<Camera>();
+			CreateSpheres();
 		}
 
 		private void UpdateRenderTexture()
@@ -39,6 +148,8 @@ namespace RT
 
 		private void OnRenderImage(RenderTexture src, RenderTexture dest)
 		{
+			AnimateSpheres();
+
 			SetShaderParameters();
 			Render(dest);
 		}
@@ -58,12 +169,17 @@ namespace RT
 		private void SetShaderParameters()
 		{
 			rtComputeShader.SetTexture(0,"skyboxTexture", skyboxTexture2D);
+
+			Vector3 lightDirection = light.transform.forward;
+			float lightIntensity = light.intensity;
+			Vector4 lightVector = new Vector4(lightDirection.x, lightDirection.y, lightDirection.z, lightIntensity);
+			
+			rtComputeShader.SetVector("directionalLight", lightVector);
 			
 			rtComputeShader.SetMatrix("cameraToWorld", camera.cameraToWorldMatrix);
 			rtComputeShader.SetMatrix("cameraInverseProjection", camera.projectionMatrix.inverse);
+			
+			rtComputeShader.SetBuffer(0, "spheres", sphereBuffer);
 		}
-		
-		
 	}
-
 }
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # GPU-Ray-Tracing
-GPU Ray Tracing realized as compute shader with help of Unity Engine
 
-Sources: http://blog.three-eyed-games.com/2018/05/03/gpu-ray-tracing-in-unity-part-1/
+[![Unity 2019.3+](https://img.shields.io/badge/unity-2020.1%2B-blue.svg)](https://unity3d.com/get-unity/download) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+GPU Ray Tracing realized as compute shader with help of Unity Engine.
+
+Based On: http://blog.three-eyed-games.com/2018/05/03/gpu-ray-tracing-in-unity-part-1/
+
+ <img src="https://i.imgur.com/mQHO0D9.png">
+ <img src="http://i.imgur.com/dcsk5KEh.gif">
